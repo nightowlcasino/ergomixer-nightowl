324a4a1cd0b451f4428551056908ef1f0e4ae32d
ethan1lad <ethanjgilder@gmail.com>
2022-02-14 22:16:59 +1100

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,8 @@
+import os
+
+headers = {
+    "api_key": os.getenv('API_KEY')
+}
+
+owl_token_id = "afd0d6cb61e86d15f2a0adc1e7e23df532ba3ff35f8ba88bed16729cae933032"
+usd_token_id = "96c402c0e658909aa03f534006124f0e43725c467dbc8dea39680d0861892de5"
